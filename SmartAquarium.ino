8589e93e9fc7ff3126f41b03daa5c97608fec0c3
hirodemon <jedfakhfekh@gmail.com>
2020-10-19 14:50:04 +0100

    Added MQTT and Temperature Sensor

--- SmartAquarium.ino
+++ SmartAquarium.ino
@@ -1,8 +1,15 @@
 #include "include/ConnectivityManager.h"
 #include "include/WaterLevelSensor.h"
+#include "include/TemperatureSensor.h"
 
-WaterLevelSensor* WLS;
 ConnectivityManager* CM;
+
+WaterLevelSensor* WLS_Level1;
+WaterLevelSensor* WLS_Level2;
+WaterLevelSensor* WLS_Level3;
+WaterLevelSensor* WLS_Level4;
+
+TemperatureSensor* TS;
 
 void setup() {
 
@@ -10,12 +17,20 @@ void setup() {
   CM->connect();
   CM->runServer();
 
-  WLS = new WaterLevelSensor(8); // 8 is the pin connected to the Water Level Sensor
+  WLS_Level1 = new WaterLevelSensor(8); // 8 is the pin connected to the Water Level Sensor
+  WLS_Level2 = new WaterLevelSensor(9); 
+  WLS_Level3 = new WaterLevelSensor(10); 
+  WLS_Level4 = new WaterLevelSensor(11); 
   
+  TS = new TemperatureSensor(A0);
 }
 
 void loop() {
   
-  WLS->update();
+  WLS_Level1->update();
+  WLS_Level2->update();
+  WLS_Level3->update();
+  WLS_Level4->update();
 
+  TS->update();
 }
--- include/ConnectivityManager.h
+++ include/ConnectivityManager.h
@@ -1,8 +1,16 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
+#include <PubSubClient.h>
 
 #define DEFAULT_SSID "TOPNETE136FE54"
 #define DEFAULT_PSWD "F57B95D1E6"
+#define DEFAULT_MQTT "broker.hivemq.com"
+
+
+void callback(char* topic, byte* payload, unsigned int length);
+WiFiClient wifiClient;
+PubSubClient client(DEFAULT_MQTT, 1883, callback, wifiClient);
+byte* payloadCallback;
 
 class ConnectivityManager{
 
@@ -12,11 +20,13 @@ protected:
 
 public:
 
-    // Static declataions, to change before connecting or running the Server
+
     // Wifi SSID
     char* ssid = DEFAULT_SSID;
     // Wifi Password
     char* password =  DEFAULT_PSWD;
+    // MQTT Server
+    char* MQTT_Server = DEFAULT_MQTT;
 
     /* Server Data Start*/
     WiFiServer Server = WiFiServer(301);
@@ -30,12 +40,12 @@ public:
         // Default constructor instructions here
     }
 
-    ConnectivityManager(char* &newSsid, char* &newPassword){
+    ConnectivityManager(char* newSsid, char* newPassword){
         ssid = newSsid;
         password = newPassword;
     }
 
-    ConnectivityManager(char* &newSsid, char* &newPassword, const uint16_t& serverPort){
+    ConnectivityManager(char* &newSsid, char* newPassword, const uint16_t& serverPort){
         ssid = newSsid;
         password = newPassword;
         Server = WiFiServer(serverPort);
@@ -65,4 +75,36 @@ public:
         Serial.print(":");
         Serial.print(Server.port());
     }
+
+    bool connectToMQTT(const char* clientName, const char* topic){
+        if(WiFi.status() == WL_CONNECTED){
+            while (!client.connected()) {
+                
+                Serial.print("Attempting MQTT connection...");
+
+                //if connected, subscribe to the topic(s) we want to be notified about
+                if (client.connect(clientName)) {
+                    Serial.print("\tMTQQ Connected");
+                    client.subscribe(topic);
+                    return true;
+                }
+                //otherwise print failed for debugging
+                else {
+                    Serial.println("\tFailed MQTT."); abort();
+                    return false;
+                }
+            }
+        }
+        return false;
+    }
+
+    void sendMQTTMessage(const char* &topic, const char* &messageToSend){
+        client.publish(topic, messageToSend, true);
+    }
+
+
 };
+
+void callback(char* topic, byte* payload, unsigned int length) {
+    payloadCallback = payload;
+}
